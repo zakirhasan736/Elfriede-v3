50420abeba0290e2ef6ddf896a6dc595101782b1
zakir <zakirhossaib736@gmail.com>
2022-12-06 04:15:56 +0600

    updated preloader added

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $('.elfriede-navigation-bar').on('click', function(){
         $('.elfriede-navigation-wrapper').addClass('info-open');
       })
-      $('.elfriede-trigger-clos').on('click', function(){
+      $('.elfriede-trigger-close').on('click', function(){
         $('.elfriede-navigation-wrapper').removeClass('info-open');
       })
 });

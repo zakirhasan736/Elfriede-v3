1ea0bfdc9c4eb543a68fb80e1151e3ab9c2965da
zakir <zakirhossaib736@gmail.com>
2022-12-06 03:56:19 +0600

    updates initial

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -6,3 +6,45 @@ $(document).ready(function() {
         $('.elfriede-navigation-wrapper').removeClass('info-open');
       })
 });
+const load = gsap.timeline({
+  paused: "true"
+})
+load.to("#percent , #bar, .loader", {
+  duration: 1,
+  opacity: 0,
+  zIndex: 1
+})
+load.to(".progress",{
+  duration: 10,
+  width: "0%"
+})
+load.from(".content", {
+  duration: .8,
+  opacity: 0,
+  ease: "Power4.out"
+}, "-=.5")
+load.from(".content h1", {
+  duration: .5,
+  y: 50,
+  skewY: 10,
+  opacity: 0
+},"-=1")
+
+var id, width = 1;
+function loading(){
+  id = setInterval(frame, 25);
+}
+function frame(){
+  if(width>=100){
+      clearInterval(id)
+      load.play();
+  }
+  else{
+      width++;
+      document.getElementById("barconfirm").style.width = width + '%';
+      document.getElementById("percent").innerHTML = width + '%';
+  }
+}
+window.onload = function(){
+  loading();
+}

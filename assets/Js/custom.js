3b71fb753eb2ad6c3046282cbd8c7fa50c986dd1
zakir <zakirhossaib736@gmail.com>
2022-12-17 00:30:20 +0600

    shope page filter function added

--- assets/Js/custom.js
+++ assets/Js/custom.js
@@ -284,8 +284,6 @@
         dots: true,
         arrows: false,
         infinite: false,
-        // nextArrow: '<i class="fas fa-angle-right"></i>',
-        // prevArrow: '<i class="fas fa-angle-left"></i>',
         autoplay: true,
         autoplaySpeed: 2000,
         cssEase: "cubic-bezier(.9, .03, .41, .49)",
@@ -376,39 +374,47 @@ $('.elfriede-singleitem-slideitem').slick({
     slidesToShow: 3,
     slidesToScroll: 3,
     responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite:true,
-          dots:false
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3
-        }
-      }
+        {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+              infinite:true,
+              dots:false,
+              centerPadding: '60px',
+    
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              centerMode: true,
+        variableWidth: true,
+        centerPadding: '60px',
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              centerMode: true,
+              variableWidth: true,
+              centerPadding: '60px',
+            }
+          }
     ]
   });
 
-   // single items slider
+   // about slider box
 $('.elfriede-slides-item').slick({
     dots:false,
     arrows: false,
     infinite:true,
     centerMode: true,
-    // variableWidth: true,
+    variableWidth: false,
     centerPadding: '180px',
     autoplay: true,
     autoplaySpeed: 2000,
@@ -420,24 +426,32 @@ $('.elfriede-slides-item').slick({
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToShow: 2,
+          slidesToScroll: 2,
           infinite:true,
-          dots:false
+          dots:false,
+          centerPadding: '60px',
+
         }
       },
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: true,
+    variableWidth: true,
+    centerPadding: '60px',
         }
       },
       {
         breakpoint: 480,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: true,
+          variableWidth: true,
+          centerPadding: '60px',
         }
       }
     ]
@@ -463,8 +477,8 @@ $('.elfriede-product-slider').slick({
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToShow: 2,
+          slidesToScroll: 2,
           infinite:true,
           dots:false
         }
@@ -472,15 +486,15 @@ $('.elfriede-product-slider').slick({
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3
+          slidesToShow: 1,
+          slidesToScroll: 1
         }
       },
       {
         breakpoint: 480,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3
+          slidesToShow: 1,
+          slidesToScroll: 1
         }
       }
     ]
@@ -494,4 +508,101 @@ $(document).ready(function() {
       $('.elfriede-trigger-close').on('click', function(){
         $('.elfriede-navigation-wrapper').removeClass('info-open');
       })
-});
+      
+
+    var buttonFilters = {};
+    var buttonFilter = '*';
+
+    var rangeFilters = {
+        'price': {'min':1000, 'max': 10000},
+        'star': {'min':1, 'max': 5}
+      };
+  
+
+    var $grid = $('.grid').isotope({
+      itemSelector: '.item',
+      layout: 'masonry',
+      filter: function() {
+        var $this = $(this);
+        var price = $this.attr('data-price');
+        var star = $this.attr('data-star');
+        var isInPriceRange = (rangeFilters['price'].min <= price && rangeFilters['price'].max >= price);
+        var isInStarRange = (rangeFilters['star'].min <= star && rangeFilters['star'].max >= star);
+
+        return $this.is( buttonFilter ) && (isInPriceRange && isInStarRange);
+      }
+    });
+  
+  
+    var $priceSlider = $('#filter-price').slider({ tooltip_split: true, min: 1000,  max: 10000, range: true, value: [1000, 10000] });
+    var $starSlider = $('#filter-star').slider({ tooltip_split: true, min: 1,  max: 5, range: true, value: [1, 5] });
+  
+  
+    function updateRangeSlider(slider, slideEvt) {
+      console.log('Current slider:' + slider);
+      var sldmin = +slideEvt.value[0],
+          sldmax = +slideEvt.value[1],
+
+          filterGroup = slider.attr('data-filter-group'),
+
+          currentSelection = sldmin + ' - ' + sldmax;
+  
+        slider.siblings('.filter-label').find('.filter-selection').text(currentSelection);
+  
+        console.log('Filtergroup: '+ filterGroup);
+        rangeFilters[filterGroup] = {
+          min: sldmin || 0,
+          max: sldmax || 100000
+        };
+
+        $grid.isotope();
+  
+    }
+  
+    $priceSlider.on('slideStop', function(slideEvt){
+      var $this =$(this);
+      updateRangeSlider($this, slideEvt);
+    });
+    $starSlider.on('slideStop', function(slideEvt){
+      var $this =$(this);
+      updateRangeSlider($this, slideEvt);
+    });
+  
+  
+    $('.filters').on( 'click', '.btn', function() {
+
+      var $this = $(this);
+      var $buttonGroup = $this.parents('.btn-group');
+
+      var filterGroup = $buttonGroup.attr('data-filter-group');
+
+      buttonFilters[ filterGroup ] = $this.attr('data-filter');
+
+      buttonFilter = concatValues( buttonFilters ) || '*';
+
+      console.log( buttonFilter )
+
+      $grid.isotope();
+    });
+  
+  
+    
+    $('.btn-group').each( function( i, buttonGroup ) {
+        var $buttonGroup = $( buttonGroup );
+        $buttonGroup.on( 'click', '.btn-filter', function() {
+            $buttonGroup.find('.is-checked').removeClass('is-checked');
+            $(this).addClass('is-checked');
+        });
+    });
+  
+  });
+  
+  // Flatten object by concatting values
+  function concatValues( obj ) {
+    var value = '';
+    for ( var prop in obj ) {
+      value += obj[ prop ];
+    }
+    return value;
+  }
+  
